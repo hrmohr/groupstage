d075fa82c079d66196ea8407b4af18703ed62ec8
Mads Mohr Christensen <hr.mohr@gmail.com>
2021-05-25 19:03:37 +0200

    initial setup

---
+++ install_mkdocs.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+cd "$(dirname "$0")" || exit
+
+# create virtualenv
+python3 -m venv venv-mkdocs
+
+# activate virtualenv and install mkdocs
+source venv-mkdocs/bin/activate
+pip install wheel
+pip install -r requirements.txt
