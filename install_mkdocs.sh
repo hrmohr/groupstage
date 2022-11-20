7e68718695f8a8173bbef3ca0ab90212b6b1207b
Mads Mohr Christensen <hr.mohr@gmail.com>
2022-11-21 00:39:40 +0100

    fix install on macos

--- install_mkdocs.sh
+++ install_mkdocs.sh
@@ -6,5 +6,6 @@ python3 -m venv venv-mkdocs
 
 # activate virtualenv and install mkdocs
 source venv-mkdocs/bin/activate
+pip install --upgrade pip
 pip install wheel
 pip install -r requirements.txt
